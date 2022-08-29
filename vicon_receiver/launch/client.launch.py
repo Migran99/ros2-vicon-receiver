ef88410c84905848b960e6e7e2507b88194b8dfe
miguel <miguelgranero99@gmail.com>
2022-08-29 13:57:47 +0200

    launch parameters

--- vicon_receiver/launch/client.launch.py
+++ vicon_receiver/launch/client.launch.py
@@ -1,13 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-
-    hostname = '192.168.2.221:801'
-    buffer_size = 4096
-    topic_namespace = 'vicon'
-
-    return LaunchDescription([Node(
+    return LaunchDescription([
+        DeclareLaunchArgument('hostname', default_value='192.168.2.221:801'),
+        DeclareLaunchArgument('buffer_size', default_value=4096),
+        DeclareLaunchArgument('namespace', default_value='vicon'),
+        Node(
             package='vicon_receiver', executable='vicon_client', output='screen',
-            parameters=[{'hostname': hostname, 'buffer_size': buffer_size, 'namespace': topic_namespace}]
+            parameters=[{
+            'hostname': LaunchConfiguration('hostname'),
+            'buffer_size': LaunchConfiguration('buffer_size'), 
+            'namespace': LaunchConfiguration('namespace')
+            }]
         )])
