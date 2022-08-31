2fb62c08f9383d90a060cd2c8bb23d13a582887d
miguel <miguelgranero99@gmail.com>
2022-08-31 11:22:24 +0200

    fixed bug in default params

--- vicon_receiver/launch/client.launch.py
+++ vicon_receiver/launch/client.launch.py
@@ -6,7 +6,7 @@ from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('hostname', default_value='192.168.2.221:801'),
-        DeclareLaunchArgument('buffer_size', default_value=4096),
+        DeclareLaunchArgument('buffer_size', default_value='4096'),
         DeclareLaunchArgument('namespace', default_value='vicon'),
         Node(
             package='vicon_receiver', executable='vicon_client', output='screen',
